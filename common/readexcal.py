4321e9f834a536ff147d2b8d4769e2a1e201cca9
jiuShangkou <xiyue@radacat.com>
2018-11-30 09:04:36 +0800

    接口自动化参考

--- common/readexcal.py
+++ common/readexcal.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 import xlrd
-class ExcalUtil():
+# class ExcalUtil():
     # excalPath : 接口用例存放位置
     # sheetName : xsl 名称
-    def __init__(self, excalPath, sheetName='Sheet1'):
+    # def __init__(self, excalPath, sheetName='Sheet1'):
